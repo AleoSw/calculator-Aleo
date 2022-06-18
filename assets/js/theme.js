ec5200cd7a1c591435630897c1c53a2a7dc86bb4
AleoSw <98068231+AleoSw@users.noreply.github.com>
2022-06-17 19:04:00 -0500

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,1 +1,19 @@
+const body = document.querySelector('body')
+const changeTheme = document.querySelector('.toggle')
 
+changeTheme.addEventListener('click', (e) => {
+    let positionToggle = e.target.id
+    let indicator = document.querySelector('.checked')
+
+    if (positionToggle === 'one') {
+        body.className = `theme-${positionToggle}`
+        indicator.style.left = '5px'
+
+    } else if (positionToggle === 'two') {
+        body.className = `theme-${positionToggle}`
+        indicator.style.left = '27px'
+    } else if (positionToggle === 'tree') {
+        body.className = `theme-${positionToggle}`
+        indicator.style.left = '50px'
+    }
+})
